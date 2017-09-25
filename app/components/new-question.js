0bc26d335dadf00c2b26af77fee267c5bf9bdc06
Philip Weru <philip.weru@gmail.com>
2017-09-25 08:33:58 +0300

    To add changes made to new-question.js

---
+++ app/components/new-question.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  action: {
+
+   newFormShow() {
+     this.set('addNewQuestion', true);
+   },
+
+   save1() {
+     var params = {
+       content: this.get('content'),
+       author: this.get('author'),
+       detail: this.get('detail')
+     },
+     this.set('content',''),
+     this.set('author',''),
+     this.set('addNewQuestion',false);
+     this.sendAction('save2',params);
+    }
+   }
+});
